a004bb8e5ceab0b54198507dd7aa9358940facbb
fakir3007 <91556351+fakir3007@users.noreply.github.com>
2022-09-04 09:55:35 +0500

    change_views

--- first_project/app/views.py
+++ first_project/app/views.py
@@ -2,34 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
 
-def home_view(request):
-    template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
-    pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
-    }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
-    context = {
-        'pages': pages
-    }
-    return render(request, template_name, context)
+def show_catalog(request):
+    template = 'catalog.html'
+    sort_pages = request.GET.get('sort')
+    phones_objects = Phone.objects.all()
+
+    if sort_pages == 'max_price':
+        phones_objects = phones_objects.order_by('price').reverse()
+    elif sort_pages == 'min_price':
+        phones_objects = phones_objects.order_by('price')
+    elif sort_pages == 'name':
+        phones_objects = phones_objects.order_by('name')
+
+    context = {'phones': phones_objects,
+               }
+    return render(request, template, context=context)
 
 
-def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
-
-
-def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+def show_product(request, slug):
+    template = 'product.html'
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    return render(request, template, context=context)
